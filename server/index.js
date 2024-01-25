9c6211931565025f7fcb129e70890a1f84027dc9
GeorgeET15 <ironmanrocks353@gmail.com>
2024-01-26 03:33:34 +0530

    Revert

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const { MongoClient } = require("mongodb");
 const { v4: uuidv4 } = require("uuid");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const nodemailer = require("nodemailer");
 require("dotenv").config();
 const uri = process.env.URI;
 const app = express();
@@ -31,76 +30,12 @@ app.get("/", (req, res) => {
   res.json("Hello to my app");
 });
 
-// app.post("/signup", async (req, res) => {
-//   const client = new MongoClient(uri);
-//   const { email, password } = req.body;
-
-//   const generatedUserId = uuidv4();
-//   const hashedPassword = await bcrypt.hash(password, 10);
-
-//   try {
-//     await client.connect();
-//     const database = client.db("app-data");
-//     const users = database.collection("users");
-
-//     const existingUser = await users.findOne({ email });
-
-//     if (existingUser) {
-//       return res.status(409).send("User already exists. Please login");
-//     }
-
-//     const sanitizedEmail = email.toLowerCase();
-
-//     const data = {
-//       user_id: generatedUserId,
-//       email: sanitizedEmail,
-//       hashed_password: hashedPassword,
-//     };
-
-//     const insertedUser = await users.insertOne(data);
-
-//     const token = jwt.sign(insertedUser, sanitizedEmail, {
-//       expiresIn: 60 * 24,
-//     });
-//     res.status(201).json({ token, userId: generatedUserId });
-//   } catch (err) {
-//     console.log(err);
-//   } finally {
-//     await client.close();
-//   }
-// });
-
-// app.post("/login", async (req, res) => {
-//   const client = new MongoClient(uri);
-//   const { email, password } = req.body;
-
-//   try {
-//     await client.connect();
-//     const database = client.db("app-data");
-//     const users = database.collection("users");
-//     const user = await users.findOne({ email });
-//     const correctPassword = await bcrypt.compare(
-//       password,
-//       user.hashed_password
-//     );
-
-//     if (user && correctPassword) {
-//       const token = jwt.sign(user, email, { expiresIn: 60 * 24 });
-//       res.status(201).json({ token, userId: user.user_id });
-//     }
-//     res.status(400).send("Invalid Credentials");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
 app.post("/signup", async (req, res) => {
   const client = new MongoClient(uri);
   const { email, password } = req.body;
 
   const generatedUserId = uuidv4();
   const hashedPassword = await bcrypt.hash(password, 10);
-  const verificationToken = uuidv4(); // Generate a unique verification token
 
   try {
     await client.connect();
@@ -110,18 +45,7 @@ app.post("/signup", async (req, res) => {
     const existingUser = await users.findOne({ email });
 
     if (existingUser) {
-      if (existingUser.verified) {
-        return res.status(409).send("User already exists. Please login");
-      } else {
-        // User exists but not verified, resend verification email if needed
-        sendVerificationEmail(
-          existingUser.email,
-          existingUser.verification_token
-        );
-        return res
-          .status(409)
-          .send("User already exists. Verification email resent");
-      }
+      return res.status(409).send("User already exists. Please login");
     }
 
     const sanitizedEmail = email.toLowerCase();
@@ -130,135 +54,42 @@ app.post("/signup", async (req, res) => {
       user_id: generatedUserId,
       email: sanitizedEmail,
       hashed_password: hashedPassword,
-      verification_token: verificationToken,
-      verified: false,
     };
 
     const insertedUser = await users.insertOne(data);
 
-    // Send verification email
-    sendVerificationEmail(sanitizedEmail, verificationToken);
-
-    res
-      .status(201)
-      .json({ message: "User created. Please verify your email." });
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      expiresIn: 60 * 24,
+    });
+    res.status(201).json({ token, userId: generatedUserId });
   } catch (err) {
     console.log(err);
-    res.status(500).send("Internal Server Error");
   } finally {
     await client.close();
   }
 });
 
-// Login route
 app.post("/login", async (req, res) => {
+  const client = new MongoClient(uri);
   const { email, password } = req.body;
 
   try {
-    const client = new MongoClient(uri);
     await client.connect();
-
     const database = client.db("app-data");
     const users = database.collection("users");
-
     const user = await users.findOne({ email });
-
-    if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    const passwordMatch = await bcrypt.compare(password, user.hashed_password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    if (!user.verified) {
-      return res
-        .status(401)
-        .json({ message: "Email not verified. Please verify your email." });
-    }
-
-    // Generate JWT token and send it in the response
-    const token = jwt.sign(
-      { user_id: user.user_id, email: user.email },
-      "your-secret-key",
-      {
-        expiresIn: 60 * 24,
-      }
+    const correctPassword = await bcrypt.compare(
+      password,
+      user.hashed_password
     );
 
-    res.status(200).json({ token, userId: user.user_id });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-  } finally {
-    await client.close();
-  }
-});
-
-function sendVerificationEmail(email, token) {
-  const transporter = nodemailer.createTransport({
-    // Configure your email provider here
-    // For example, using Gmail:
-    service: "gmail",
-    auth: {
-      user: "unidate2024@gmail.com",
-      pass: "UniDateForever@2024",
-    },
-  });
-
-  const verificationLink = `https://uni-date-app.onrender.com/verify-email?token=${token}`;
-
-  const mailOptions = {
-    from: "unidate2024@gmail.com",
-    to: email,
-    subject: "Email Verification",
-    text: `Click the following link to verify your email: ${verificationLink}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
+    if (user && correctPassword) {
+      const token = jwt.sign(user, email, { expiresIn: 60 * 24 });
+      res.status(201).json({ token, userId: user.user_id });
     }
-  });
-}
-
-app.get("/verify-email", async (req, res) => {
-  const { token } = req.query;
-
-  if (!token) {
-    return res.status(400).send("Invalid verification link");
-  }
-
-  try {
-    const client = new MongoClient(uri);
-    await client.connect();
-
-    const database = client.db("app-data");
-    const users = database.collection("users");
-
-    const user = await users.findOne({ verification_token: token });
-
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-
-    if (user.verified) {
-      return res.status(200).send("Email already verified");
-    }
-
-    // Mark the user as verified in the database
-    await users.updateOne({ _id: user._id }, { $set: { verified: true } });
-
-    res.status(200).send("Email verification successful. You can now log in.");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  } finally {
-    await client.close();
+    res.status(400).send("Invalid Credentials");
+  } catch (error) {
+    console.log(error);
   }
 });
 
