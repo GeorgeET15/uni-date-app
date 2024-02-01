4e5c61d2d136d866497cd7d63298898b067dc5ed
GeorgeET15 <ironmanrocks353@gmail.com>
2024-02-01 21:08:32 +0530

    ss

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import CountdownTimer from "./components/CountdownTimer";
-import UserCount from "./components/UserCount";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <div
+    <div
       style={{
         display: "flex",
         flexDirection: "column",
@@ -17,7 +16,6 @@ root.render(
       }}
     >
       <CountdownTimer />
-    </div> */}
-    <App />
+    </div>
   </React.StrictMode>
 );
