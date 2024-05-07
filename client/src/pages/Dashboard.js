cc9cc2a68974d2dad47758b17c51318b06622057
George Emmanuel Thomas <72024420+GeorgeET15@users.noreply.github.com>
2024-05-07 10:39:36 +0530

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import CircularProgress from "@mui/material/CircularProgress";
 import Drawer from "@mui/material/Drawer";
 import TinderCard from "react-tinder-card";
-import ChatContainer from "../components/ChatContainer";
-import { useCookies } from "react-cookie";
-import axios from "axios";
-import CircularProgress from "@mui/material/CircularProgress";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-} from "@mui/material";
-import Typography from "@mui/material/Typography";
+import ChatContainer from "../components/ChatContainer";
 import colorImage from "../images/logo.png";
 import UserCount from "../components/UserCount";
 
@@ -32,14 +25,37 @@ const Dashboard = () => {
 
   const userId = cookies.UserId;
 
+  useEffect(() => {
+    const fetchTotalUsers = async () => {
+      try {
+        const response = await fetch("https://uni-date-app.onrender.com/total-registered-users");
+        const data = await response.json();
+        if (response.ok) {
+          setTotalUsers(data.totalRegisteredUsers);
+          setMaleUsers(data.maleUsers);
+          setFemaleUsers(data.femaleUsers);
+          console.log(data);
+        } else {
+          console.error("Error fetching total registered users:", data.error);
+        }
+      } catch (error) {
+        console.error("Error fetching total registered users:", error.message);
+      }
+    };
+
+    fetchTotalUsers();
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      getGenderedUsers();
+    }
+  }, [user]);
+
   const getUser = async () => {
     try {
-      const response = await axios.get(
-        "https://uni-date-app.onrender.com/user",
-        {
-          params: { userId },
-        }
-      );
+      const response = await axios.get("https://uni-date-app.onrender.com/user", { params: { userId } });
       setUser(response.data);
     } catch (error) {
       console.log(error);
@@ -50,31 +66,15 @@ const Dashboard = () => {
 
   const getGenderedUsers = async () => {
     try {
-      const response = await axios.get(
-        "https://uni-date-app.onrender.com/gendered-users",
-        {
-          params: { gender: user?.gender_interest },
-        }
-      );
-
-      // Reverse the order of the array
+      const response = await axios.get("https://uni-date-app.onrender.com/gendered-users", {
+        params: { gender: user?.gender_interest },
+      });
       const reversedUsers = response.data.slice().reverse();
-
       setGenderedUsers(reversedUsers);
     } catch (error) {
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  useEffect(() => {
-    if (user) {
-      getGenderedUsers();
-    }
-  }, [user]);
 
   const updateMatches = async (matchedUserId, isLiked) => {
     try {
@@ -91,9 +91,9 @@ const Dashboard = () => {
 
   const swiped = (direction, swipedUserId) => {
     if (direction === "right") {
-      updateMatches(swipedUserId, true); // Liked
+      updateMatches(swipedUserId, true);
     } else if (direction === "left") {
-      updateMatches(swipedUserId, false); // Disliked
+      updateMatches(swipedUserId, false);
     }
     setLastDirection(direction);
   };
@@ -102,9 +102,7 @@ const Dashboard = () => {
     console.log(name + " left the screen!");
   };
 
-  const matchedUserIds = user?.matches
-    .map(({ user_id }) => user_id)
-    .concat(userId);
+  const matchedUserIds = user?.matches?.map(({ user_id }) => user_id).concat(userId);
 
   const filteredGenderedUsers = genderedUsers?.filter(
     (genderedUser) =>
@@ -116,15 +114,10 @@ const Dashboard = () => {
     setChatDrawerOpen(!isChatDrawerOpen);
   };
 
-  const isMobile = window.innerWidth <= 768;
-
   const handleOpenMilestonesDialog = () => {
-    // Show circular progress for 2 seconds
     setMilestonesDialogOpen(true);
     setLoadingD(true);
-
     setTimeout(() => {
-      // After 2 seconds, set the loading state to false and open the dialog
       setLoadingD(false);
     }, 3000);
   };
@@ -133,169 +126,132 @@ const Dashboard = () => {
     setMilestonesDialogOpen(false);
   };
 
-  useEffect(() => {
-    const fetchTotalUsers = async () => {
-      try {
-        const response = await fetch(
-          "https://uni-date-app.onrender.com/total-registered-users"
-        );
-        const data = await response.json();
-
-        if (response.ok) {
-          setTotalUsers(data.totalRegisteredUsers);
-          setMaleUsers(data.maleUsers);
-          setFemaleUsers(data.femaleUsers);
-          console.log(data);
-        } else {
-          console.error("Error fetching total registered users:", data.error);
-        }
-      } catch (error) {
-        console.error("Error fetching total registered users:", error.message);
-      }
-    };
-
-    fetchTotalUsers();
-  }, []);
-
-
+  const isMobile = window.innerWidth <= 768;
 
   return (
-  <>
-    {loading && (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <CircularProgress style={{ color: "#a27ef8" }} />
-      </div>
-    )}
+    <>
+      {loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress style={{ color: "#a27ef8" }} />
+        </div>
+      )}
 
-    {!loading && user && (
-      <div className="dashboard">
-        {isMobile ? (
-          <>
-            <div className="show-info-button">
-              <InfoIcon onClick={handleOpenMilestonesDialog} />
-            </div>
-            <button
-              className="show-matches-button"
-              onClick={toggleChatDrawer}
-            >
-              Show Matches
-            </button>
-            <Drawer
-              anchor="bottom"
-              open={isChatDrawerOpen}
-              onClose={toggleChatDrawer}
-              PaperProps={{
-                style: {
-                  width: "100%",
-                  height: "100%",
-                },
-              }}
-            >
-              <ChatContainer user={user} onCloseDrawer={toggleChatDrawer} />
-            </Drawer>
-          </>
-        ) : (
-          <ChatContainer user={user} />
-        )}
-
-        <div className="swipe-container">
-          <div className="card-container">
-            {filteredGenderedUsers && filteredGenderedUsers.length > 0 ? (
-              filteredGenderedUsers.map((genderedUser) => (
-                <TinderCard
-                  className="swipe"
-                  key={genderedUser.user_id}
-                  onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
-                  onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}
-                >
-                  <Dialog
-                    open={isMilestonesDialogOpen}
-                    onClose={handleCloseMilestonesDialog}
-                    BackdropProps={{
-                      style: {
-                        backgroundColor: "rgba(0, 0, 0, 0)", // Completely transparent
-                      },
-                    }}
-                  >
-                    <DialogTitle>More Info</DialogTitle>
-                    <DialogContent>
-                      {loadingD ? (
-                        <div
-                          style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            height: "100px", // Adjust the height as needed
-                          }}
-                        >
-                          <CircularProgress style={{ color: "#a27ef8" }} />
-                        </div>
-                      ) : (
-                        <>
-                          <Typography variant="h5" sx={{ marginBottom: 2 }}>
-                            {genderedUser?.first_name +
-                              " " +
-                              genderedUser?.last_name}
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            Gender: {genderedUser?.gender_identity}
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            Age: {2024 - genderedUser?.dob_year}
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            Branch: {genderedUser?.branch}
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            Year: {genderedUser?.current_year}
-                          </Typography>
-                          <Typography variant="body1" gutterBottom>
-                            About: {genderedUser?.about}
-                          </Typography>
-                        </>
-                      )}
-                    </DialogContent>
-
-                    <DialogActions>
-                      <Button
-                        sx={{ color: "#7f4ef6" }}
-                        onClick={handleCloseMilestonesDialog}
-                      >
-                        Close
-                      </Button>
-                    </DialogActions>
-                  </Dialog>
-                  <div
-                    style={{ backgroundImage: `url(${genderedUser.url})` }}
-                    className="card"
-                  >
-                    <h3>
-                      {genderedUser.first_name},{" "}
-                      {2024 - genderedUser.dob_year}
-                    </h3>
-                  </div>
-                </TinderCard>
-              ))
-            ) : (
-              <div className="no-cards-message">
-                <img src={colorImage} alt="image" />
-                <p>No more cards to display</p>
+      {!loading && user && (
+        <div className="dashboard">
+          {isMobile ? (
+            <>
+              <div className="show-info-button">
+                <InfoIcon onClick={handleOpenMilestonesDialog} />
               </div>
-            )}
+              <button className="show-matches-button" onClick={toggleChatDrawer}>
+                Show Matches
+              </button>
+              <Drawer
+                anchor="bottom"
+                open={isChatDrawerOpen}
+                onClose={toggleChatDrawer}
+                PaperProps={{
+                  style: {
+                    width: "100%",
+                    height: "100%",
+                  },
+                }}
+              >
+                <ChatContainer user={user} onCloseDrawer={toggleChatDrawer} />
+              </Drawer>
+            </>
+          ) : (
+            <ChatContainer user={user} />
+          )}
+
+          <div className="swipe-container">
+            <div className="card-container">
+              {filteredGenderedUsers && filteredGenderedUsers.length > 0 ? (
+                filteredGenderedUsers.map((genderedUser) => (
+                  <TinderCard
+                    className="swipe"
+                    key={genderedUser.user_id}
+                    onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
+                    onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}
+                  >
+                    <Dialog
+                      open={isMilestonesDialogOpen}
+                      onClose={handleCloseMilestonesDialog}
+                      BackdropProps={{
+                        style: {
+                          backgroundColor: "rgba(0, 0, 0, 0)",
+                        },
+                      }}
+                    >
+                      <DialogTitle>More Info</DialogTitle>
+                      <DialogContent>
+                        {loadingD ? (
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center",
+                              height: "100px",
+                            }}
+                          >
+                            <CircularProgress style={{ color: "#a27ef8" }} />
+                          </div>
+                        ) : (
+                          <>
+                            <Typography variant="h5" sx={{ marginBottom: 2 }}>
+                              {genderedUser?.first_name + " " + genderedUser?.last_name}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                              Gender: {genderedUser?.gender_identity}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                              Age: {2024 - genderedUser?.dob_year}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                              Branch: {genderedUser?.branch}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                              Year: {genderedUser?.current_year}
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                              About: {genderedUser?.about}
+                            </Typography>
+                          </>
+                        )}
+                      </DialogContent>
+                      <DialogActions>
+                        <Button sx={{ color: "#7f4ef6" }} onClick={handleCloseMilestonesDialog}>
+                          Close
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                    <div
+                      style={{ backgroundImage: `url(${genderedUser.url})` }}
+                      className="card"
+                    >
+                      <h3>{genderedUser.first_name}, {2024 - genderedUser.dob_year}</h3>
+                    </div>
+                  </TinderCard>
+                ))
+              ) : (
+                <div className="no-cards-message">
+                  <img src={colorImage} alt="image" />
+                  <p>No more cards to display</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </>
-)
-
-
+      )}
+    </>
+  );
+};
 
 export default Dashboard;
